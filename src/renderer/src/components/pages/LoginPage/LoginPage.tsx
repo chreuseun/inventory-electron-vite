2be565dc702675f14f1906fc47fbd7cd41abbf85
chreuseun <eunillevillegaseunille@gmail.com>
2024-12-08 15:38:28 +0800

    add md:

--- src/renderer/src/components/pages/LoginPage/LoginPage.tsx
+++ src/renderer/src/components/pages/LoginPage/LoginPage.tsx
@@ -32,7 +32,7 @@ const LoginPage: React.FC = () => {
 
   return (
     <div className="h-full overflow-y-scroll p-4 flex justify-end gap-x-4">
-      <div className="bg-secondaryBackground rounded-xl p-5 px-10 w-1/3 max-w-xl pt-56 flex flex-col items-stretch">
+      <div className="bg-secondaryBackground rounded-xl p-5 px-10 md:w-1/3 max-w-xl pt-56 flex flex-col items-stretch">
         <MyTextInput placeholder="Username" />
         <div className="mb-2"></div>
         <MyTextInput placeholder="Password" type="password" />
