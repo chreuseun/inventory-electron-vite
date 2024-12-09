c9fd7b36d691be3f97444a74f8e80004dd817f7c
chreuseun <eunillevillegaseunille@gmail.com>
2024-12-09 09:04:47 +0800

    add toast in login

--- src/renderer/src/components/pages/LoginPage/LoginPage.tsx
+++ src/renderer/src/components/pages/LoginPage/LoginPage.tsx
@@ -1,13 +1,27 @@
 import { MyTextInput } from '@renderer/components/common'
 import MyButton from '@renderer/components/common/MyButton'
 import { useVerifyUser } from '@renderer/hooks/users'
+import { showToast } from '@renderer/utils/reactToastify'
 import { useState } from 'react'
 
 const LoginPage: React.FC = () => {
   const [username, setUN] = useState('')
   const [password, setPW] = useState('')
 
-  const { runVerifyUser } = useVerifyUser({})
+  const { runVerifyUser } = useVerifyUser({
+    onError: (errMsg) => {
+      showToast({
+        message: errMsg,
+        type: 'warning'
+      })
+    },
+    onCompleted: () => {
+      showToast({
+        message: `Login Successful`,
+        type: 'success'
+      })
+    }
+  })
 
   const onClickLogin: () => void = () => {
     runVerifyUser({
@@ -28,9 +42,18 @@ const LoginPage: React.FC = () => {
     }
   }
 
+  const handleKeyDown: React.KeyboardEventHandler<HTMLDivElement> = (event) => {
+    if (event.key === 'Enter') {
+      onClickLogin()
+    }
+  }
+
   return (
     <div className="h-full overflow-y-scroll p-4 flex justify-end gap-x-4">
-      <div className="bg-secondaryBackground rounded-xl p-5 px-10 md:w-1/3 max-w-xl pt-56 flex flex-col items-stretch">
+      <div
+        className="bg-secondaryBackground rounded-xl p-5 px-10 md:w-1/3 max-w-xl pt-56 flex flex-col items-stretch"
+        onKeyDown={handleKeyDown}
+      >
         <MyTextInput placeholder="Username" onChange={onChangeUsernameInput} />
         <div className="mb-2"></div>
         <MyTextInput placeholder="Password" type="password" onChange={onChangePasswordInput} />
