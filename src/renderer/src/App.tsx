8bd4df116b3818f66ce3b70ab936d4960aede579
chreuseun <eunillevillegaseunille@gmail.com>
2024-12-09 09:04:41 +0800

    import ToastContainer

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,11 +2,13 @@ import { Provider } from 'react-redux'
 
 import store from '@renderer/redux/store'
 import AuthWrapperRoute from './routers'
+import { ToastContainer } from 'react-toastify'
 
 function App(): JSX.Element {
   return (
     <Provider store={store}>
       <AuthWrapperRoute />
+      <ToastContainer />
     </Provider>
   )
 }
