3b8ae90e5323ba56f1615ff3a55250ab7607de7a
chreuseun <eunillevillegaseunille@gmail.com>
2024-12-09 09:56:06 +0800

    Update App Router

--- src/renderer/src/routers/ApplicationRouter.tsx
+++ src/renderer/src/routers/ApplicationRouter.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { HashRouter as Router, Route, Routes, Navigate } from 'react-router'
 
-import { MaterialsMainPage, HomePage } from '@renderer/components/pages'
+import { MaterialsMainPage } from '@renderer/components/pages'
 import { MySidebar } from '@renderer/components/common'
 import { APPLICATION_ROUTES } from '@renderer/configs/applicationRouter.config'
 
@@ -12,9 +12,11 @@ const PublicRouter: React.FC = () => {
       <div className="h-full overflow-y-scroll flex justify-start bg-secondaryBackground">
         <MySidebar />
         <Routes>
-          <Route path={APPLICATION_ROUTES.HOME.path} element={<HomePage />} />
           <Route path={APPLICATION_ROUTES.MANAGE_MATERIALS.path} element={<MaterialsMainPage />} />
-          <Route path="*" element={<Navigate to={APPLICATION_ROUTES.HOME.path} replace />} />
+          <Route
+            path="*"
+            element={<Navigate to={APPLICATION_ROUTES.MANAGE_MATERIALS.path} replace />}
+          />
         </Routes>
       </div>
     </Router>
