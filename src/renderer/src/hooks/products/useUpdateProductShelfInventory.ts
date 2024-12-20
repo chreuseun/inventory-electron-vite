e031287a3c54c8cc93ffcaccde11fa5ca87a1ca9
chreuseun <eunillevillegaseunille@gmail.com>
2024-12-21 00:24:38 +0800

    Add insert to shelf_stock_transaction table whenever we remove items in product.shelf_stock

--- src/renderer/src/hooks/products/useUpdateProductShelfInventory.ts
+++ src/renderer/src/hooks/products/useUpdateProductShelfInventory.ts
@@ -1,6 +1,13 @@
+import {
+  ShelfTransactionTypes,
+  StandardInventoryIntention,
+  StandardInventoryReason,
+  StandardTransactors
+} from '@renderer/interfaces/inventroy.interface'
 import { handleError } from '@renderer/utils/api'
 import { showToast } from '@renderer/utils/reactToastify'
 import { executeSQLiteQuery, ISqliteUpdateResponse } from '@renderer/utils/sqlite'
+import { generateUUID } from '@renderer/utils/uuid'
 import { useState } from 'react'
 
 const MINUS_PRODUCT_SHELF_BY_ID = `
@@ -11,13 +18,27 @@ WHERE
   id=@productID
 `
 
-// const ADD_PRODUCT_SHELF_BY_ID = `
-// UPDATE products
-// SET
-// 	shelf_quantity=shelf_quantity + @shelfQuantity
-// WHERE
-//   id=@productID
-// `
+const INSERT_DEBIT_SHELF_TRANSACTION = `
+INSERT INTO shelf_stock_transactions (
+reference_id,
+  product_id,
+  quantity,
+  transaction_type,
+  created_by,
+  transacted_by,
+  intention,
+  reason
+) VALUES (
+  @referenceID, 
+  @productID,
+  @shelfQuantity,
+  @transactionType,
+  @createdBy,
+  @transactedBy,
+  @reason,
+  @intention
+)
+`
 
 type IRunUpdateProductShelfInventory = (arg: {
   productID: string
@@ -45,19 +66,40 @@ const useUpdateProductShelfInventory: UseUpdateProductShelfInventory = (props) =
         operationName: 'runUpdateProductShelfInventory',
         action: 'update'
       })) as ISqliteUpdateResponse
-      response.result
 
       if (response.error) {
-        handleError(response.error, props?.onError)
-      } else {
-        showToast({
-          type: `success`,
-          message: `Product Shelf Stock Updated`
-        })
+        throw new Error(response.error)
+      }
 
-        if (props?.onCompleted) {
-          props.onCompleted(response)
-        }
+      const addToShelfTxnResponse = (await executeSQLiteQuery({
+        sql: INSERT_DEBIT_SHELF_TRANSACTION,
+        params: [
+          {
+            referenceID: generateUUID(),
+            productID: Number(args.productID).toFixed(),
+            shelfQuantity: args.shelfQuantity,
+            transactionType: ShelfTransactionTypes.OUT,
+            createdBy: StandardTransactors.SYSTEM,
+            transactedBy: StandardTransactors.SYSTEM,
+            reason: StandardInventoryReason.SALE,
+            intention: StandardInventoryIntention.PURCHASE
+          }
+        ],
+        operationName: 'addToShelfResponse',
+        action: 'create'
+      })) as ISqliteUpdateResponse
+
+      if (addToShelfTxnResponse.error) {
+        throw new Error(addToShelfTxnResponse.error)
+      }
+
+      showToast({
+        type: `success`,
+        message: `Product Shelf Stock Updated`
+      })
+
+      if (props?.onCompleted) {
+        props.onCompleted(response)
       }
     } catch (sqlError) {
       handleError(`${sqlError}`, props?.onError)
--- src/renderer/src/interfaces/inventroy.interface.ts
+++ src/renderer/src/interfaces/inventroy.interface.ts
@@ -2,3 +2,26 @@ export enum InventoryType {
   SHELF = 'SHELF',
   POTENTIAL = 'POTENTIAL'
 }
+
+export enum ShelfTransactionTypes {
+  IN = 'IN',
+  OUT = 'OUT'
+}
+
+export enum StandardTransactors {
+  ADMIN = 'ADMIN',
+  SYSTEM = 'SYSTEM'
+}
+
+export enum StandardInventoryReason {
+  Damaged = 'Damaged',
+  EXPIRED = 'Expired',
+  SALE = 'Sale',
+  RESTOCK_SHELF = 'Restock Shelf'
+}
+
+export enum StandardInventoryIntention {
+  ADJUSTMENT = 'ADJUSTMENT',
+  TRANSFER = 'TRANSFER',
+  PURCHASE = 'PURCHASE'
+}
